b640141701a5f139721e3e245c6bc2e9be2f79d2
Qingwan-cn <75924250+Qingwan-cn@users.noreply.github.com>
2022-05-01 21:18:37 +0800

    Add files via upload

---
+++ src/Application.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+
+/**
+ * @author XingYuan Guo
+ */
+public class Application {
+
+    public static void main(String[] args) throws IOException {
+        new ServerListener().start();
+    }
+
+
+}
